94cc340bd749b09286b11db53de8132cb3a635c4
dsrishi <dsrishi92@gmail.com>
2025-04-15 21:32:49 +0530

    analysis

--- src/components/analysis/BasicStats.tsx
+++ src/components/analysis/BasicStats.tsx
@@ -1,9 +1,13 @@
-import { useEffect, useState } from "react";
-import { Editor, JSONContent } from "@tiptap/react";
+import { Editor } from "@tiptap/react";
 import { Speech } from "@/types/speech";
 import { useUser } from "@/contexts/UserContext";
 import { useTheme } from "@/contexts/ThemeContext";
-import { estimatedDuration } from "@/helpers/speechHelpers";
+import {
+  getCharacterCount,
+  getEstimatedDuration,
+  getParagraphCount,
+  getWordCount,
+} from "@/helpers/speechHelpers";
 
 interface BasicStatsProps {
   editor: Editor | null;
@@ -13,39 +17,6 @@ interface BasicStatsProps {
 export default function BasicStats({ editor, speechData }: BasicStatsProps) {
   const { theme } = useTheme();
   const { user } = useUser();
-  const [wordCount, setWordCount] = useState(0);
-
-  // Update on every editor change
-  useEffect(() => {
-    if (!editor) return;
-
-    const updateStats = () => {
-      const content = editor.getHTML();
-      setWordCount(getWordCount(content));
-    };
-
-    // Subscribe to editor changes
-    editor.on("update", updateStats);
-
-    // Initial update
-    updateStats();
-
-    // Cleanup
-    return () => {
-      editor.off("update", updateStats);
-    };
-  }, [editor]);
-
-  const getWordCount = (content: string | JSONContent | JSONContent[]) => {
-    // Convert JSONContent to string
-    const contentString =
-      typeof content === "string" ? content : JSON.stringify(content);
-    const tempElement = document.createElement("div");
-    tempElement.innerHTML = contentString;
-    const textContent = tempElement.textContent || tempElement.innerText || "";
-    const words = textContent.trim().split(/\s+/);
-    return words.length;
-  };
 
   return (
     <div className="flex flex-col gap-6">
@@ -59,7 +30,7 @@ export default function BasicStats({ editor, speechData }: BasicStatsProps) {
           <h3 className="text-lg font-semibold mb-2">Word Count</h3>
           <div className="space-y-1">
             <p>Planned Count: {speechData.word_count || "-"}</p>
-            <p>Estimated Count: {wordCount}</p>
+            <p>Estimated Count: {getWordCount(editor?.getHTML() || "")}</p>
           </div>
         </div>
 
@@ -74,8 +45,24 @@ export default function BasicStats({ editor, speechData }: BasicStatsProps) {
             <p>Planned Duration: {speechData.duration || "-"} min</p>
             <p>
               Estimated Duration:{" "}
-              {estimatedDuration(wordCount, user?.speaking_pace || 140)}
+              {getEstimatedDuration(
+                getWordCount(editor?.getHTML() || ""),
+                user?.speaking_pace || 140
+              )}
             </p>
+          </div>
+        </div>
+
+        <div
+          className="p-6 rounded"
+          style={{
+            backgroundColor: theme === "dark" ? "#2d2d2d" : "#f5f5f5",
+          }}
+        >
+          <h3 className="text-lg font-semibold mb-2">Stats</h3>
+          <div className="space-y-1">
+            <p>Characters: {getCharacterCount(editor?.getHTML() || "")}</p>
+            <p>Paragraphs: {getParagraphCount(editor?.getHTML() || "")}</p>
           </div>
         </div>
       </div>
--- src/helpers/speechHelpers.ts
+++ src/helpers/speechHelpers.ts
@@ -1,5 +1,7 @@
+import { JSONContent } from "@tiptap/react";
+
 // function to calculate the duration
-export function estimatedDuration(words: number, wordsPerMinute: number) {
+export function getEstimatedDuration(words: number, wordsPerMinute: number) {
   const duration = Math.floor(words / wordsPerMinute);
   const seconds = Math.floor((words % wordsPerMinute) * 60 / wordsPerMinute);
   if (duration > 0) {
@@ -8,3 +10,32 @@ export function estimatedDuration(words: number, wordsPerMinute: number) {
     return `${seconds} sec`;
   }
 }
+
+// function to calculate the word count
+export function getWordCount(content: string | JSONContent | JSONContent[]) {
+  // Convert JSONContent to string
+  const contentString =
+    typeof content === "string" ? content : JSON.stringify(content);
+  const tempElement = document.createElement("div");
+  tempElement.innerHTML = contentString;
+  const textContent = tempElement.textContent || tempElement.innerText || "";
+  const words = textContent.trim().split(/\s+/);
+  return words.length;
+};
+
+// function to calculate characters count
+export function getCharacterCount(content: string | JSONContent | JSONContent[]) {
+  const contentString =
+    typeof content === "string" ? content : JSON.stringify(content);
+  return contentString.length;
+}
+
+// function to calculate paragraphs count
+export function getParagraphCount(content: string | JSONContent | JSONContent[]) {
+  const contentString =
+    typeof content === "string" ? content : JSON.stringify(content);
+  //any other way to do this other than splitting by new line?
+  console.log(contentString);
+
+  return contentString.split("<p></p>").length;
+  }
